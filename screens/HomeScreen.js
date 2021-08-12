0da240481873d27975f5aa08bfc38e58a01895d1
nilesh-05 <nilesh0411.cse19@chitkara.edu.in>
2021-08-12 17:11:09 +0530

    setting up home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
+import CustomListItem from "../components/CustomListItem";
+import { auth, db } from "../firebase";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: "Signal",
+			headerStyle: { backgroundColor: "#fff" },
+			headerTitleStyle: { color: "black" },
+			headerTintColor: "black",
+			headerLeft: () => (
+				<View style={{ marginLeft: 20 }}>
+					<Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+				</View>
+			),
+		});
+	}, []);
 	return (
-		<View>
-			<Text>Home Screen</Text>
-		</View>
+		<SafeAreaView>
+			<ScrollView>
+				<CustomListItem />
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
